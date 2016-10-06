f30e5e28841317d81763d5d873bbfe4260c111da
otis chen <otischen@otisde-MacBook-Pro.local>
2016-10-06 23:19:34 +0800

    Impelemt input widget.

--- app/controllers/home_controller_decorator.rb
+++ app/controllers/home_controller_decorator.rb
@@ -2,11 +2,17 @@ Spree::HomeController.class_eval do
   skip_before_filter :verify_authenticity_token, :only => [:add_to_cart]
   def add_to_cart
     if params[:items]
+
       order = current_order(create_order_if_necessary: true)
+
+      puts '----------------'
+      puts order.inspect
+      puts '----------------'
+
       items = eval(params[:items])
 
       (0..items.length - 1).step(2).each do |index|
-        if order.line_items.find_by_variant_id(items[index]) && items[index+1] > 0
+        if !order.line_items.find_by_variant_id(items[index]) && items[index+1] > 0
           order.contents.add(Spree::Variant.find(items[index]), items[index+1], {})
         elsif (order.line_items.find_by_variant_id(items[index]) &&
           order.line_items.find_by_variant_id(items[index]).quantity != items[index+1])
--- app/overrides/add_batch_product_to_cart.rb
+++ app/overrides/add_batch_product_to_cart.rb
@@ -8,11 +8,7 @@ Deface::Override.new(:virtual_path => 'spree/shared/_products',
   :text => '<% if product.can_supply? %>
           <div class="add-to-cart">
             <div class="input-group" style="width: 100%">
-              <% if current_order && current_order.line_items.find_by_variant_id(product.id) %>
-                <%= number_field_tag :quantity, current_order.line_items.find_by_variant_id(product.id).quantity, class: "title form-control product-number", min: 0, :variant_id => product.id%>
-              <% else %>
-                <%= number_field_tag :quantity, 0, class: "title form-control product-number", min: 0, :variant_id => product.id%>
-              <% end %>
+              <%= render :partial => "spree/home/input_widget", :locals => { :product => product } %>
             </div>
           </div>
         <% end %>'
@@ -47,6 +43,79 @@ Deface::Override.new(:virtual_path => 'spree/home/index',
               }
 
               $(document).ready(function(){
+                $(".btn-number").click(function(e){
+                    e.preventDefault();
+
+                    fieldName = $(this).attr("data-field");
+                    type      = $(this).attr("data-type");
+                    var input = $("input[name=\'"+fieldName+"\']");
+                    var currentVal = parseInt(input.val());
+                    if (!isNaN(currentVal)) {
+                        if(type == "minus") {
+
+                            if(currentVal > input.attr("min")) {
+                                input.val(currentVal - 1).change();
+                            }
+                            if(parseInt(input.val()) == input.attr("min")) {
+                                $(this).attr("disabled", true);
+                            }
+
+                        } else if(type == "plus") {
+
+                            if(currentVal < input.attr("max")) {
+                                input.val(currentVal + 1).change();
+                            }
+                            if(parseInt(input.val()) == input.attr("max")) {
+                                $(this).attr("disabled", true);
+                            }
+
+                        }
+                    } else {
+                        input.val(0);
+                    }
+                });
+                $(".input-number").focusin(function(){
+                   $(this).data("oldValue", $(this).val());
+                });
+                $(".input-number").change(function() {
+
+                  minValue =  parseInt($(this).attr("min"));
+                  maxValue =  parseInt($(this).attr("max"));
+                  valueCurrent = parseInt($(this).val());
+
+                  name = $(this).attr("name");
+                  if(valueCurrent >= minValue) {
+                      $(".btn-number[data-type=\'minus\'][data-field=\'"+name+"\']").removeAttr("disabled")
+                  } else {
+                      alert("Sorry, the minimum value was reached");
+                      $(this).val($(this).data("oldValue"));
+                  }
+                  if(valueCurrent <= maxValue) {
+                      $(".btn-number[data-type=\'plus\'][data-field=\'"+name+"\']").removeAttr("disabled")
+                  } else {
+                      alert("Sorry, the maximum value was reached");
+                      $(this).val($(this).data("oldValue"));
+                  }
+
+
+                });
+                $(".input-number").keydown(function (e) {
+                    // Allow: backspace, delete, tab, escape, enter and .
+                    if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+                         // Allow: Ctrl+A
+                        (e.keyCode == 65 && e.ctrlKey === true) ||
+                         // Allow: home, end, left, right
+                        (e.keyCode >= 35 && e.keyCode <= 39)) {
+                             // let it happen, don"t do anything
+                             return;
+                    }
+                    // Ensure that it is a number and stop the keypress
+                    if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+                        e.preventDefault();
+                    }
+                });
+
+
                 $(".add_to_cart").click(function() {
                   var items = $(".product-number").map(function() {
                     return [$(this).attr("variant_id"), $(this).val()];
