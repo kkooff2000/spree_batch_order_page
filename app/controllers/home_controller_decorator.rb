2380be4314bb3739288af74dc980102228d1ad19
otis chen <otischen@otisde-MacBook-Pro.local>
2016-10-06 17:15:49 +0800

    Implement batch order on homepage.

--- app/controllers/home_controller_decorator.rb
+++ app/controllers/home_controller_decorator.rb
@@ -1,18 +1,21 @@
 Spree::HomeController.class_eval do
+  skip_before_filter :verify_authenticity_token, :only => [:add_to_cart]
   def add_to_cart
-    puts '---------------------'
-    puts params.inspect
-    puts '---------------------'
     if params[:items]
-      order = current_order(create_order_if_necessary: true)
-      params[:items].each do |item|
+      order = current_order
+      items = eval(params[:items])
 
+      (0..items.length - 1).step(2).each do |index|
+        if !current_order.line_items.find_by_variant_id(items[index]) && items[index+1] > 0
+          order.contents.add(Spree::Variant.find(items[index]), items[index+1], {})
+        elsif (current_order.line_items.find_by_variant_id(items[index]) &&
+          current_order.line_items.find_by_variant_id(items[index]).quantity != items[index+1])
+            order.contents.add(Spree::Variant.find(items[index]), items[index+1] - current_order.line_items.find_by_variant_id(items[index]).quantity, {})
+        end
       end
 
     end
 
-    respond_to do |format|
-      format.json { render :json => {:result => 'success'}.to_json }
-    end
+    redirect_to :back
   end
 end
--- app/overrides/add_batch_product_to_cart.rb
+++ app/overrides/add_batch_product_to_cart.rb
@@ -8,7 +8,11 @@ Deface::Override.new(:virtual_path => 'spree/shared/_products',
   :text => '<% if product.can_supply? %>
           <div class="add-to-cart">
             <div class="input-group" style="width: 100%">
-              <%= number_field_tag :quantity, 0, class: "title form-control product-number", min: 0, :variant_id => product.master.id%>
+              <% if current_order && current_order.line_items.find_by_variant_id(product.id) %>
+                <%= number_field_tag :quantity, current_order.line_items.find_by_variant_id(product.id).quantity, class: "title form-control product-number", min: 0, :variant_id => product.id%>
+              <% else %>
+                <%= number_field_tag :quantity, 0, class: "title form-control product-number", min: 0, :variant_id => product.id%>
+              <% end %>
             </div>
           </div>
         <% end %>'
@@ -21,21 +25,34 @@ Deface::Override.new(:virtual_path => 'spree/home/index',
             <br>
             <br>
             <script>
+              function post(path, params, method) {
+                method = method || "post";
+                var form = document.createElement("form");
+                form.setAttribute("method", method);
+                form.setAttribute("action", path);
+
+                for(var key in params) {
+                  if(params.hasOwnProperty(key)) {
+                    var hiddenField = document.createElement("input");
+                    hiddenField.setAttribute("type", "hidden");
+                    hiddenField.setAttribute("name", key);
+                    hiddenField.setAttribute("value", params[key]);
+
+                    form.appendChild(hiddenField);
+                   }
+                }
+
+                document.body.appendChild(form);
+                form.submit();
+              }
+
               $(document).ready(function(){
                 $(".add_to_cart").click(function() {
-                  var items = $(".product-number").map(function() {return [$(this).attr("variant_id"), $(this).val()];}).toArray();
-                  console.log(items);
-                  $.ajax({
-                    method: "POST",
-                    url: "home/add_to_cart",
-                    dataType: "jsonp",
-                    data: {items: items}
-                  }).success(function() {
-                      console.log("success");
-                    }).fail(function(e) {
-                      console.log("error");
-                      console.log(e);
-                    });
+                  var items = $(".product-number").map(function() {
+                    return [$(this).attr("variant_id"), $(this).val()];
+                  }).toArray();
+
+                  post("/home/add_to_cart", {items: "["+items+"]"}, "post");
                 });
               });
             </script>'
